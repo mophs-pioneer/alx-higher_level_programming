f91f048b0824e4c8625e5fd21d1e199158e8a58f
philip <kabagiphilip9708@gmail.com>
2023-08-28 12:26:18 -0700

    m exception classes in python:

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
